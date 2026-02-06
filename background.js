6812502364c79c0e8fcdfed328f27a9a49884352
rohitssingh17 <rohitshekrsingh@gmail.com>
2026-02-07 02:50:28 +0530

    changes

--- background.js
+++ background.js
@@ -1,4 +1,25 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Hiredd Extension Installed");
   });
+
+
+  const callWeb = async (userEmail) => {
+   
+  }
+
+
+
+  chrome.runtime.onStartup.addListener(async () => {
+    const URL = `https://system-backend-hprl.onrender.com/api/get-user-by-email?email=rohitshekrsingh@gmail.com`;
   
+    const response = await fetch(URL);
+    
+    const data = await response.json();
+  
+    console.log(data);
+  
+    // Save data in extension storage
+    await chrome.storage.local.set({ startupInfo: data });
+  
+    console.log("Saved data to storage:", data);
+  });
--- popup.js
+++ popup.js
@@ -1,13 +1,21 @@
-chrome.storage.local.get("jobData", (result) => {
-  const status = document.getElementById("status");
-  const role = document.getElementById("role");
-  const company = document.getElementById("company");
+// Ask the current tab to analyze the page when popup loads
+document.addEventListener("DOMContentLoaded", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "checkForJobPage" });
+  });
+});
 
-  if (result.jobData) {
-    status.textContent = "Job page detected";
-    role.textContent = "Role: " + result.jobData.role;
-    company.textContent = "Company: " + result.jobData.company;
-  } else {
-    status.textContent = "Not a job page";
+// Listen for the response from content.js
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  const statusElement = document.getElementById("status");
+
+  if (message.jobPage !== undefined) {
+    if (message.jobPage) {
+      statusElement.textContent = "Hiredd found a Job!";
+      statusElement.style.color = "green";
+    } else {
+      statusElement.textContent = "No job content found.";
+      statusElement.style.color = "red";
+    }
   }
 });
